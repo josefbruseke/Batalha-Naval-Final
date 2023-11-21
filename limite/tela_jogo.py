d2dbe281ae130a6ad2cb473d8e87827add9851e9
Rafael Bleyer Zanette Furtado <rafinhabzf@gmail.com>
2023-11-21 01:15:12 -0300

    organizando telas na branch

--- limite/tela_jogo.py
+++ limite/tela_jogo.py
@@ -1,4 +1,4 @@
-"""class TelaJogo:
+class TelaJogo:
     def recebe_login(self):
         print("------LOGIN-------")
         recebe_nome = input("Digite seu nome: ")
@@ -17,82 +17,31 @@
         print("1 - Iniciar partida")
         print("2 - Ver ranking")
         print("0 - Voltar")
+        print("---------------------")
         opcao = int(input("Escolha a opção: "))
         return opcao
 
+    def mostra_resultados(self, duracao, vencedor, pontuacao_jogador, pontuacao_computador):
+        print("------RESULTADOS------")
+        print(f"Duração da partida: {duracao}")
+        print(f"O vencedor da partida foi o: {vencedor}")
+        print(f"Pontuação do jogador: {pontuacao_jogador}")
+        print(f"Pontuação do computador: {pontuacao_computador}")
+        print("---------------------")
+    
+    def mostra_opcoes_final(self):
+        print("------------------------------")
+        print("Selecione a sua opção final:")
+        print("1 - Iniciar um novo jogo")
+        print("2 - Voltar para o Menu Jogo")
+        print("0 - Encerrar o sistema")
+        print("------------------------------")
+        opcao = int(input("Escolhe a opção: "))
+        return opcao
 
     def mostra_mensagem(self, msg):
         print(msg)
 
     def voltar(self):
-        opcao = input("Deseja voltar? [S/N]").upper()
-        return opcao"""
-        
-import PySimpleGUI as sg
-
-class TelaJogo:
-    def __init__(self):
-        sg.theme('DarkBlue')
-
-    def recebe_login(self):
-        layout = [
-            [sg.Text('------LOGIN-------')],
-            [sg.Text('Digite seu nome:'), sg.Input(key='-NOME-')],
-            [sg.Text('Digite sua senha:'), sg.Input(key='-SENHA-', password_char='*')],
-            [sg.Button('Login')],
-        ]
-
-        window = sg.Window('Login', layout)
-
-        while True:
-            event, values = window.read()
-
-            if event == sg.WIN_CLOSED:
-                break
-
-            if event == 'Login':
-                nome = values['-NOME-']
-                senha = values['-SENHA-']
-                window.close()
-                return {"recebe_nome": nome, "recebe_senha": senha}
-
-    def mostra_resultado_jogo(self):
-        pass
-
-    def mostra_resultado_rodada(self, jogador, resultado):
-        sg.popup(f"{jogador} {resultado} o tiro")
-
-    def mostra_opcoes(self):
-        layout = [
-            [sg.Text('------MENU JOGO------')],
-            [sg.Text('Selecione a opção desejada')],
-            [sg.Button('Iniciar partida')],
-            [sg.Button('Ver ranking')],
-            [sg.Button('Voltar')],
-        ]
-
-        window = sg.Window('Menu Jogo', layout)
-
-        while True:
-            event, values = window.read()
-
-            if event == sg.WIN_CLOSED:
-                break
-
-            if event == 'Iniciar partida':
-                window.close()
-                return 1
-            elif event == 'Ver ranking':
-                window.close()
-                return 2
-            elif event == 'Voltar':
-                window.close()
-                return 0
-
-    def mostra_mensagem(self, msg):
-        sg.popup(msg)
-
-    def voltar(self):
-        opcao = sg.popup_yes_no('Deseja voltar?')
-        return opcao == 'Yes'
-        
+        opcao = input("Deseja voltar para o meu inicial? [S/N] ").upper()
+        return opcao
--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -1,48 +1,9 @@
-"""class TelaSistema:
+class TelaSistema:
     def mostra_opcoes(self):
-        print("------Batalha Naval------")
+        print("------MENU INICIAL------")
         print("Selecione a opção desejada")
-        print("1 - Fazer Login")
-        print("2 - Fazer Cadastro")
-        print("3 - Remover Cadastro")
+        print("1 - Login")
+        print("2 - Cadastro")
         print("0 - Encerrar o sistema")
         opcao = int(input("Escolha a opção: "))
         return opcao
-"""
-
-import PySimpleGUI as sg
-
-class TelaSistema:
-    def mostra_opcoes(self):
-        sg.theme('DarkBlue')
-        layout = [
-            [sg.Text('------Batalha Naval------')],
-            [sg.Text('Selecione a opção desejada')],
-            [sg.Button('Fazer Login')],
-            [sg.Button('Fazer Cadastro')],
-            [sg.Button('Remover Cadastro')],
-            [sg.Button('Alterar Cadastro')],
-            [sg.Button('Encerrar o sistema')],
-        ]
-
-        window = sg.Window('Batalha Naval', layout)
-
-        while True:
-            event, values = window.read()
-            if event == sg.WIN_CLOSED:
-                break
-            if event == 'Fazer Login':
-                window.close()
-                return 1
-            elif event == 'Fazer Cadastro':
-                window.close()
-                return 2
-            elif event == 'Remover Cadastro':
-                window.close()
-                return 3
-            elif event == 'Alterar Cadastro':
-                window.close()
-                return 4
-            elif event == 'Encerrar o sistema':
-                window.close()
-                return 0
