df3accdbfd60b6a6162cfb47c0342c8071e3700f
Josef Bruseke <72322400+josefbruseke@users.noreply.github.com>
2023-11-21 09:57:53 -0300

    remove coment

--- limite/tela_embarcacao.py
+++ limite/tela_embarcacao.py
@@ -11,29 +11,6 @@ class TelaEmbarcacao:
         coluna = input("Coluna: ").upper()
         return linha, coluna
         
-        '''
-        print("Qual embarcação você deseja posicionar?")
-        print("Escolha uma das opções:")
-        print("1 - Bote")
-        print("2 - Submarino")
-        print("3 - Fragata")
-        print("4 - Bote")
-        posicao = int(input("Opção número: "))
-        if posicao == 1:
-            print("Escolha 1 posição para inserir o Bote: ")
-            coluna = input("Informe a coluna: ")
-            linha = int(input("Informe a linha: "))
-        elif posicao == 2:
-            print("Escolha 2 posições para inserir o Submarino: ")
-            coluna = input("Informe a coluna: ")
-            linha = int(input("Informe a linha: "))
-        elif posicao == 3:
-            print("Escolha 3 posições para inserir a Fragata: ")
-        elif posicao == 4:
-            print("Escolha 4 posições para inserir o Porta-aviões: ")
-        else:
-            print("Opção inválida!")
-        '''
     
     def mostra_embarcacoes_disponiveis(self, embarcacoes):
         pass
