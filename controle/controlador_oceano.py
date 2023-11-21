895ace918dc4f4e633d594383ef3914757a2cd7f
Josef Bruseke <72322400+josefbruseke@users.noreply.github.com>
2023-11-21 09:59:59 -0300

    add tratamento de excessao

--- controle/controlador_oceano.py
+++ controle/controlador_oceano.py
@@ -5,11 +5,13 @@ from entidade.fragata import Fragata
 from entidade.porta_avioes import PortaAvioes
 from entidade.bote import Bote
 from entidade.submarino import Submarino
+from controle.controlador_excessao import ControladorExcessao
 
 class ControladorOceano:
     def __init__(self, controlador_sistema):
         self.__controlador_sistema = controlador_sistema
         self.__tela_embarcacao = TelaEmbarcacao()
+        self.__controlador_excessao = ControladorExcessao()
         self.__tela_oceano = TelaOceano()
         self.__embarcacoes = [
             PortaAvioes(7),
@@ -21,8 +23,20 @@ class ControladorOceano:
 
     
     def recebe_tamanho_oceano(self):
-        tamanho = self.__tela_oceano.recebe_tamanho()
-        return tamanho
+        while True:
+            try:
+                tamanho = self.__tela_oceano.recebe_tamanho()
+                if not(tamanho > 26 or tamanho < 6):
+                    return tamanho
+                else:
+                    self.__tela_oceano.mostra_mensagem("Tamanho inválido!")
+                    self.__tela_oceano.mostra_mensagem("Forneça um tamanho entre 6 e 26.")
+                    self.recebe_tamanho_oceano()
+            except Exception as e:
+                mensagem = "Digite um número válido para o tamanho do oceano"
+                self.__controlador_excessao.handle_value_error(e, mensagem)
+                self.recebe_tamanho_oceano()
+
         
 
     def cria_oceano(self, tamanho):
