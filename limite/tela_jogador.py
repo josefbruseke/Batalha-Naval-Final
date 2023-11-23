0efffe005d03f6c737f4920c6ab428aadd10b1ca
Josef Bruseke <josefbruseke@gmail.com>
2023-11-23 09:19:29 -0300

    add ocoes alteracao

--- limite/tela_jogador.py
+++ limite/tela_jogador.py
@@ -7,7 +7,6 @@ class TelaJogador:
         return {"nome": nome, "data_nascimento": data_nascimento, "senha": senha}
     
     def seleciona_jogador(self):
-        print("------REMOVE CADASTRO------")
         nome = input("Digite o nome do Jogador: ")
         senha = input("Digite a senha: ")
         return {"nome": nome, "senha": senha}
@@ -23,7 +22,27 @@ class TelaJogador:
         opcao = int(input("Escolha a opção: "))
         return opcao
 
+    def opcoes_alteracao(self):
+        print("-------ALTERAÇÃO-------")
+        print("1 - Alterar senha")
+        print("2 - Alterar nome")
+        print("3 - Alterar data de nascimento")
+        print("0 - voltar")
+        print("-----------------------")
+        opcao = int(input("Escolha a opção: "))
+        return opcao
+    
+    def recebe_nova_senha(self):
+        nova_senha = input("Digite sua nova senha: ")
+        return nova_senha
+    
+    def recebe_novo_nome(self):
+        novo_nome = input("Digite seu novo nome: ")
+        return novo_nome
 
+    def recebe_nova_data_nascimento(self):
+        nova_data_nascimento = input("Digite sua nova data_de_nascimento: ")
+        return nova_data_nascimento
 
     def mostra_historico(self):
         pass
