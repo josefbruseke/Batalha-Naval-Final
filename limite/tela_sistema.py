dc87425e5b73ac619e1cb31558d8eda7220464c2
Josef Bruseke <josefbruseke@gmail.com>
2023-11-23 13:36:19 -0300

    fix menu inicial

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -25,10 +25,11 @@ class TelaSistema:
             [sg.Text('Selecione a opção desejada')],
             [sg.Button('Login', key='1')],
             [sg.Button('Cadastro', key='2')],
+            [sg.Button('Ranking', key='3')],
             [sg.Button('Encerrar o sistema', key='0')],
         ]
 
-        window = sg.Window('Menu Inicial', layout)
+        window = sg.Window('Menu Inicial', layout, size=(600, 400), grab_anywhere=True)
 
         while True:
             event, values = window.read()
@@ -36,7 +37,7 @@ class TelaSistema:
             if event == sg.WIN_CLOSED:
                 break
 
-            if event in ['1', '2', '0']:
+            if event in ['1', '2', '3', '0']:
                 window.close()
                 return int(event)
 
