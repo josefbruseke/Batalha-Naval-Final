353a4186b5171b98d8c7a190271bbc5d3b12ab4c
Josef Bruseke <72322400+josefbruseke@users.noreply.github.com>
2023-11-24 23:15:37 -0300

    remove attribute

--- entidade/oceano.py
+++ entidade/oceano.py
@@ -5,11 +5,7 @@ class Oceano:
     def __init__(self, tamanho_oceano: int, matriz, embarcacoes):
         self.__tamanho_oceano = tamanho_oceano
         self.__matriz = matriz
-        self.__embarbacacoes = embarcacoes
 
-    @property
-    def embarcacoes(self):
-        return self.__embarbacacoes
     
     @property
     def tamanho_oceano(self):
