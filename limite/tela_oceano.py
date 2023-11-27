32379bacb4867d802c6875fd5ca91440f4107743
Josef Bruseke <josefbruseke@gmail.com>
2023-11-27 17:48:09 -0300

    add changes

--- limite/tela_oceano.py
+++ limite/tela_oceano.py
@@ -7,18 +7,22 @@ class TelaOceano:
     def recebe_tamanho(self):
         layout = [
             [sg.Text('Digite o tamanho do oceano:'), sg.Input(key='-TAMANHO-')],
-            [sg.Button('OK')],
+            [sg.Button('OK'), sg.Button('Voltar')],
         ]
 
-        window = sg.Window('Tamanho do Oceano', layout, auto_size_text=True, auto_size_buttons=True)
+        window = sg.Window('Tamanho do Oceano', layout, auto_size_text=True, auto_size_buttons=True, element_justification='center')
 
         while True:
             event, values = window.read()
 
             if event == sg.WIN_CLOSED:
                 window.close()
-                return None
-
+                return 'Fechar'
+            
+            if event == 'Voltar':
+                window.close()
+                return 'Voltar'
+    
             if event == 'OK':
                 try:
                     tamanho = int(values['-TAMANHO-'])
